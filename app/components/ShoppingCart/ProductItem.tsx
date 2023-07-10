7733464deb92780562b463765698fcb92fdaa978
Frank Licona <3l33f3@gmail.com>
2023-07-10 13:24:21 -0500

    add responsive details & icons SVG components

--- app/components/ShoppingCart/ProductItem.tsx
+++ app/components/ShoppingCart/ProductItem.tsx
@@ -6,6 +6,7 @@ import TextLabel from "@components/TextLabel";
 import TextLabelItalic from "@components/TextLabelItalic";
 import utils from "@utils/index";
 import { TOrderProduct } from "../../types/SushiCartTypes";
+import { CrossIcon, MinusIcon, PlusIcon } from "@components/Icons";
 
 type TProductItemProps = {
   item: TOrderProduct;
@@ -26,14 +27,8 @@ export default function ProductItem({ item }: TProductItemProps) {
         onClick={() => handleDeleteProduct(product)}
         className="absolute z-10 right-4 top-4"
       >
-        <div className="fill-white hover:fill-slate-500 transition-colors ">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1.75em"
-            viewBox="0 0 384 512"
-          >
-            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-          </svg>
+        <div className="fill-white hover:fill-red-200 transition-colors ">
+          <CrossIcon/>
         </div>
       </button>
       <div className="flex justify-between">
@@ -47,16 +42,10 @@ export default function ProductItem({ item }: TProductItemProps) {
           </LabelsWrapper>
           <button
             onClick={() => handleRemoveProduct(product)}
-            className="bg-slate-500 w-8 hover:bg-slate-300 rounded-full aspect-square flex justify-center items-center"
+            className="bg-slate-500 w-8 hover:bg-slate-300 transition-colors rounded-full aspect-square flex justify-center items-center"
           >
             <span className="fill-white p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                viewBox="0 0 448 512"
-              >
-                <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z" />
-              </svg>
+              <MinusIcon/>
             </span>
           </button>
           <span className="text-slate-800 w-8 text-2xl rounded-full bg-slate-300 mx-2 flex justify-center items-center ">
@@ -64,16 +53,10 @@ export default function ProductItem({ item }: TProductItemProps) {
           </span>
           <button
             onClick={() => handleAddProduct(product)}
-            className="bg-slate-500 w-8 hover:bg-slate-300 rounded-full aspect-square flex justify-center items-center"
+            className="bg-slate-500 w-8 hover:bg-slate-300 transition-colors rounded-full aspect-square flex justify-center items-center"
           >
             <span className="fill-white p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                viewBox="0 0 448 512"
-              >
-                <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-              </svg>
+              <PlusIcon/>
             </span>
           </button>
         </div>
--- app/components/TextDescription.tsx
+++ app/components/TextDescription.tsx
@@ -5,12 +5,12 @@ export default function TextDescription({ text, isModal }: TLabelProps) {
   return (
     <div
       className={`absolute right-0 bottom-0 w-2/3 max-h-16 md:max-h-32 lg:max-h-fit    overflow-hidden  text-slate-100 bg-black/25 p-1 lg:p-4 ${
-        isModal ? "sm:max-w-2/3 sm:p-2" : "sm:max-w-sm sm:p-2"
+        isModal ? "sm:max-w-2/3 lg:p-2 lg:max-w-lg " : "sm:max-w-sm  sm:p-2"
       }`}
     >
       <p
-        className={` text-sm lg:text-4xl font-black ${
-          isModal ? "md:text-3xl" : "md:text-lg"
+        className={` text-sm lg:text-2xl xl:text-5xl font-black ${
+          isModal ? "md:text-3xl lg:text-xl sm:font-bold" : "md:text-lg"
         } `}
       >
         {text}
