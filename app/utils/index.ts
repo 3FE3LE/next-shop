16cd3683679a7ed00fbc9a9cf6897c4f061d847e
Frank Licona <3l33f3@gmail.com>
2023-07-09 21:48:05 -0500

    config tailwind.config.js to add custom rules, add import aliases for constants & utils

---
+++ app/utils/index.ts
@@ -0,0 +1,11 @@
+const utils = {
+  formatPrice(price: number)  {
+    const formattedPrice = price.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    return formattedPrice;
+  }
+}
+
+export default utils;
