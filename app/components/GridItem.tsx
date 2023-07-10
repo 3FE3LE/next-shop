bb99e7aa4920d919c10c8e7ede5aae1f99018bc3
Frank Licona <3l33f3@gmail.com>
2023-07-10 12:37:40 -0500

    add label wraper & ProductItem components, responsive desing to grid & grid items

--- app/components/GridItem.tsx
+++ app/components/GridItem.tsx
@@ -5,13 +5,14 @@ import TextLabel from "./TextLabel";
 import TextLabelItalic from "./TextLabelItalic";
 import utils from "@utils/index";
 import TextDescription from "./TextDescription";
+import LabelsWrapper from "./LabelsWrapper";
 
 type GridItemProps = {
   item: TProduct;
 };
 
 export default function GridItem({ item }: GridItemProps) {
-  const { handleAddProduct } = useSushiCart();
+  const { handleAddProduct, isModalShow } = useSushiCart();
 
   return (
     <div
@@ -20,9 +21,14 @@ export default function GridItem({ item }: GridItemProps) {
         item.color + " first:col-span-2 first:row-span-2 aspect-square relative"
       }
     >
-      <TextLabel text={item.name} />
-      <TextLabelItalic text={utils.formatPrice(item.price)} />
-      <TextDescription text={item.description} />
+      <LabelsWrapper>
+        <TextLabel isModal={isModalShow} text={item.name} />
+        <TextLabelItalic
+          isModal={isModalShow}
+          text={utils.formatPrice(item.price)}
+        />
+      </LabelsWrapper>
+      <TextDescription isModal={isModalShow} text={item.description} />
     </div>
   );
 }
--- app/components/TextDescription.tsx
+++ app/components/TextDescription.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { TLabelProps } from "../types/SushiCartTypes";
 
-export default function TextDescription({ text }: TLabelProps) {
+export default function TextDescription({ text, isModal }: TLabelProps) {
   return (
-    <div className="absolute right-0 bottom-0 max-w-sm text-slate-100 bg-black/25 p-4">
-      <p className="text-4xl font-black ">{text}</p>
+    <div
+      className={`absolute right-0 bottom-0 w-2/3 max-h-16 md:max-h-32 lg:max-h-fit    overflow-hidden  text-slate-100 bg-black/25 p-1 lg:p-4 ${
+        isModal ? "sm:max-w-2/3 sm:p-2" : "sm:max-w-sm sm:p-2"
+      }`}
+    >
+      <p
+        className={` text-sm lg:text-4xl font-black ${
+          isModal ? "md:text-3xl" : "md:text-lg"
+        } `}
+      >
+        {text}
+      </p>
     </div>
   );
 }
