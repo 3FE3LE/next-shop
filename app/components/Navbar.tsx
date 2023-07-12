eb443528f20122427abfb15180236a8172e7b279
Frank Licona <3l33f3@gmail.com>
2023-07-11 20:40:15 -0500

    rename, relocate & refactor components to article detail page integration

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,36 +2,36 @@
 import useSushiCart from "@hooks/useSushiCart";
 import Link from "next/link";
 import { CartIcon } from "./Icons";
+import ShoppingCart from "./ShoppingCart";
 
 export default function Navbar() {
   const { handleChangeModal, products } = useSushiCart();
 
   return (
-    <nav className="flex sticky top-0 justify-between p-4 h-18">
-      <div className="flex gap-8 items-center">
-        <h1 className="font-bold text-2xl">Sushi Shop</h1>
-        <Link href={"/total"}>
-          <li>Total</li>
-        </Link>
-        <Link href={"/resume"}>
-          <li>Resume</li>
-        </Link>
-      </div>
-      <button className="relative" onClick={() => handleChangeModal()}>
-        <div className="absolute -right-2 -top-2  flex">
-          <span
-            className={`transition-transform ${
-              products.length > 0 ? "scale-100" : "scale-0"
-            }
-             text-white font-bold text-xs m-auto bg-blue-500 rounded-full first-line:h-4 w-4`}
-          >
-            {products.length}
-          </span>
+    <>
+      <nav className="flex sticky top-0 justify-between p-4 h-18 z-40 bg-black">
+        <div className="flex gap-8 items-center">
+          <Link href={"/"}>
+            <h1 className="font-bold text-2xl">Sushi Shop</h1>
+          </Link>
         </div>
-        <div className="fill-white hover:fill-slate-500 hover:scale-110 transition">
-          <CartIcon/>
-        </div>
-      </button>
-    </nav>
+        <button className="relative" onClick={() => handleChangeModal()}>
+          <div className="absolute -right-2 -top-2  flex">
+            <span
+              className={`transition-transform ${
+                products.length > 0 ? "scale-100" : "scale-0"
+              }
+            text-white font-bold text-xs m-auto bg-blue-500 rounded-full first-line:h-4 w-4`}
+            >
+              {products.length}
+            </span>
+          </div>
+          <div className="fill-white hover:fill-slate-500 hover:scale-110 transition">
+            <CartIcon />
+          </div>
+        </button>
+        <ShoppingCart />
+      </nav>
+    </>
   );
 }
--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,469 +1,499 @@
 export const sushiPlates = [
   {
-    name:{
+    id: "1a",
+    name: {
       es: "California Roll",
       en: "California Roll",
-    } ,
+    },
     price: 8.99,
     img: "california_roll.jpg",
     description: {
-      es:"Roll de cangrejo, aguacate y pepino",
+      es: "Roll de cangrejo, aguacate y pepino",
       en: "Crab stick, avocado, and cucumber roll",
     },
-    categoryId: 1,
-    color: "bg-red-500"
+    category: "sushi_rolls",
+    color: "bg-red-500",
   },
   {
-    name:{
+    id: "2b",
+    name: {
       es: "Sashimi Variado",
       en: "Assorted Sashimi",
-    } ,
+    },
     price: 12.99,
     img: "sashimi.jpg",
     description: {
-      es:"Variado de láminas frescas de salmón, atún y jurel",
+      es: "Variado de láminas frescas de salmón, atún y jurel",
       en: "Assortment of fresh slices of salmon, tuna, and yellowtail",
     },
-    categoryId: 1,
-    color: "bg-red-500"
+    category: "sushi_rolls",
+    color: "bg-red-500",
   },
   {
-    name:{
+    id: "3c",
+    name: {
       es: "Nigiri de Salmón",
       en: "Salmon Nigiri",
-    } ,
+    },
     price: 4.99,
     img: "nigiri_salmon.jpg",
     description: {
-      es:"Láminas frescas de salmón sobre arroz sazonado",
+      es: "Láminas frescas de salmón sobre arroz sazonado",
       en: "Fresh salmon slices over seasoned rice",
     },
-    categoryId: 1,
-    color: "bg-red-500"
+    category: "sushi_rolls",
+    color: "bg-red-500",
   },
   {
-    name:{
+    id: "4d",
+    name: {
       es: "Maki de Atún",
       en: "Tuna Maki",
-    } ,
+    },
     price: 6.99,
     img: "atun_maki.jpg",
     description: {
-      es:"Maki de atún fresco y aguacate envuelto en alga nori y arroz",
+      es: "Maki de atún fresco y aguacate envuelto en alga nori y arroz",
       en: "Fresh tuna and avocado wrapped in nori seaweed and rice",
     },
-    categoryId: 1,
-    color: "bg-red-500"
+    category: "sushi_rolls",
+    color: "bg-red-500",
   },
   {
-    name:{
+    id: "5e",
+    name: {
       es: "Uramaki Tempura",
       en: "Tempura Uramaki",
-    } ,
+    },
     price: 9.99,
     img: "urumaki_tempura.jpg",
     description: {
-      es:"Uramaki frito relleno de langostinos tempura, aguacate y queso crema",
+      es: "Uramaki frito relleno de langostinos tempura, aguacate y queso crema",
       en: "Deep-fried uramaki filled with tempura shrimp, avocado, and cream cheese",
     },
-    categoryId: 1,
-    color: "bg-red-500"
+    category: "sushi_rolls",
+    color: "bg-red-500",
   },
   {
-    name:{
+    id: "6f",
+    name: {
       es: "Gyoza de Cerdo",
       en: "Pork Gyoza",
-    } ,
+    },
     price: 5.99,
     img: "pig_gyoza.jpg",
     description: {
-      es:"Empanadillas japonesas rellenas de cerdo, cocidas al vapor y luego doradas",
+      es: "Empanadillas japonesas rellenas de cerdo, cocidas al vapor y luego doradas",
       en: "Japanese dumplings filled with pork, steamed and then pan-fried",
     },
-    categoryId: 2,
-    color: "bg-cyan-600"
+    category: "hot_appetizers",
+    color: "bg-cyan-600",
   },
   {
-    name:{
+    id: "7g",
+    name: {
       es: "Yakitori de Pollo",
       en: "Chicken Yakitori",
-    } ,
+    },
     price: 4.99,
     img: "chiken_yakitori.jpg",
     description: {
-      es:"Brochetas de pollo asado a la parrilla con salsa teriyaki",
+      es: "Brochetas de pollo asado a la parrilla con salsa teriyaki",
       en: "Grilled skewers of chicken with teriyaki sauce",
     },
-    categoryId: 2,
-    color: "bg-cyan-600"
+    category: "hot_appetizers",
+    color: "bg-cyan-600",
   },
   {
-    name:{
+    id: "8h",
+    name: {
       es: "Tempura de Vegetales",
       en: "Vegetable Tempura",
-    } ,
+    },
     price: 6.99,
     img: "vegetables_tempura.jpg",
     description: {
-      es:"Vegetales variados en tempura, fritos hasta obtener una textura crujiente",
+      es: "Vegetales variados en tempura, fritos hasta obtener una textura crujiente",
       en: "Assorted vegetables in tempura, fried to a crispy texture",
     },
-    categoryId: 2,
-    color: "bg-cyan-600"
+    category: "hot_appetizers",
+    color: "bg-cyan-600",
   },
   {
-    name:{
+    id: "9i",
+    name: {
       es: "Sopa Miso",
       en: "Miso Soup",
-    } ,
+    },
     price: 3.99,
     img: "miso_soup.jpg",
     description: {
-      es:"Sopa tradicional japonesa a base de pasta de miso, tofu y algas",
+      es: "Sopa tradicional japonesa a base de pasta de miso, tofu y algas",
       en: "Traditional Japanese soup made with miso paste, tofu, and seaweed",
     },
-    categoryId: 2,
-    color: "bg-cyan-600"
+    category: "hot_appetizers",
+    color: "bg-cyan-600",
   },
   {
-    name:{
+    id: "10j",
+    name: {
       es: "Tataki de Res",
       en: "Beef Tataki",
-    } ,
+    },
     price: 10.99,
     img: "tataki_meat.jpg",
     description: {
-      es:"Finas láminas de res selladas y marinadas con salsa de soja y jengibre",
+      es: "Finas láminas de res selladas y marinadas con salsa de soja y jengibre",
       en: "Thinly sliced seared beef marinated in soy sauce and ginger",
     },
-    categoryId: 2,
-    color: "bg-cyan-600"
+    category: "hot_appetizers",
+    color: "bg-cyan-600",
   },
   {
-    name:{
+    id: "11k",
+    name: {
       es: "Tartar de Salmón",
       en: "Salmon Tartare",
-    } ,
+    },
     price: 9.99,
     img: "tartar_salmon.jpg",
     description: {
-      es:"Tartar de salmón fresco con aguacate, cebolla morada y aderezo de limón",
+      es: "Tartar de salmón fresco con aguacate, cebolla morada y aderezo de limón",
       en: "Fresh salmon tartare with avocado, red onion, and lemon dressing",
     },
-    categoryId: 3,
-    color: "bg-sky-500"
+    category: "sashimi_specialties",
+    color: "bg-sky-500",
   },
   {
-    name:{
+    id: "12l",
+    name: {
       es: "Tataki de Atún",
       en: "Tuna Tataki",
-    } ,
+    },
     price: 12.99,
     img: "tataki_atun.jpg",
     description: {
-      es:"Láminas de atún selladas y marinadas en salsa de soja y sésamo",
+      es: "Láminas de atún selladas y marinadas en salsa de soja y sésamo",
       en: "Seared slices of tuna marinated in soy sauce and sesame",
     },
-    categoryId: 3,
-    color: "bg-sky-500"
+    category: "sashimi_specialties",
+    color: "bg-sky-500",
   },
   {
-    name:{
+    id: "13m",
+    name: {
       es: "Nigiri de Pulpo",
       en: "Octopus Nigiri",
-    } ,
+    },
     price: 5.99,
     img: "niguiri_octopus.jpg",
     description: {
-      es:"Láminas de pulpo tierno y fresco sobre arroz sazonado",
+      es: "Láminas de pulpo tierno y fresco sobre arroz sazonado",
       en: "Tender and fresh octopus slices over seasoned rice",
     },
-    categoryId: 3,
-    color: "bg-sky-500"
+    category: "sashimi_specialties",
+    color: "bg-sky-500",
   },
   {
-    name:{
+    id: "14n",
+    name: {
       es: "Maki de Pepino",
       en: "Cucumber Maki",
-    } ,
+    },
     price: 4.99,
     img: "maki_cucumber.jpg",
     description: {
-      es:"Maki de pepino fresco envuelto en alga nori y arroz",
+      es: "Maki de pepino fresco envuelto en alga nori y arroz",
       en: "Fresh cucumber wrapped in nori seaweed and rice",
     },
-    categoryId: 3,
-    color: "bg-sky-500"
+    category: "sashimi_specialties",
+    color: "bg-sky-500",
   },
   {
-    name:{
+    id: "15o",
+    name: {
       es: "Uramaki Rainbow",
       en: "Rainbow Uramaki",
-    } ,
+    },
     price: 9.99,
     img: "urumaki_rainbow_sushi.jpg",
     description: {
-      es:"Uramaki relleno de langostino, aguacate y cubierto con láminas de pescado y vegetalesvariados",
+      es: "Uramaki relleno de langostino, aguacate y cubierto con láminas de pescado y vegetales variados",
       en: "Uramaki filled with shrimp, avocado, and topped with assorted fish slices and vegetables",
     },
-    categoryId: 3,
-    color: "bg-sky-500"
+    category: "sashimi_specialties",
+    color: "bg-sky-500",
   },
   {
-    name:{
+    id: "16p",
+    name: {
       es: "Tacos de Salmón",
       en: "Salmon Tacos",
-    } ,
+    },
     price: 7.99,
     img: "tacos_salmon.jpg",
     description: {
-      es:"Tacos de salmón fresco con salsa de chipotle y repollo rallado",
+      es: "Tacos de salmón fresco con salsa de chipotle y repollo rallado",
       en: "Fresh salmon tacos with chipotle sauce and shredded cabbage",
     },
-    categoryId: 4,
-    color: "bg-lime-400"
+    category: "fusion_dishes",
+    color: "bg-lime-400",
   },
   {
-    name:{
+    id: "17q",
+    name: {
       es: "Ceviche de Atún",
       en: "Tuna Ceviche",
-    } ,
+    },
     price: 8.99,
     img: "ceviche_atun.jpg",
     description: {
-      es:"Ceviche de atún fresco marinado en limón con cebolla roja y cilantro",
+      es: "Ceviche de atún fresco marinado en limón con cebolla roja y cilantro",
       en: "Fresh tuna ceviche marinated in lime juice with red onion and cilantro",
     },
-    categoryId: 4,
-    color: "bg-lime-400"
+    category: "fusion_dishes",
+    color: "bg-lime-400",
   },
   {
-    name:{
+    id: "18r",
+    name: {
       es: "Rollito Vietnamita",
       en: "Vietnamese Spring Roll",
-    } ,
+    },
     price: 5.99,
     img: "vietnamitian_roll.jpg",
     description: {
-      es:"Rollito de papel de arroz relleno de camarones, fideos de arroz y vegetales",
+      es: "Rollito de papel de arroz relleno de camarones, fideos de arroz y vegetales",
       en: "Rice paper roll filled with shrimp, rice noodles, and vegetables",
     },
-    categoryId: 4,
-    color: "bg-lime-400"
+    category: "fusion_dishes",
+    color: "bg-lime-400",
   },
   {
-    name:{
+    id: "19s",
+    name: {
       es: "Ensalada de Algas",
       en: "Seaweed Salad",
-    } ,
+    },
     price: 6.99,
     img: "seaweed_salad.jpg",
     description: {
-      es:"Ensalada fresca de algas con aderezo de sésamo",
+      es: "Ensalada fresca de algas con aderezo de sésamo",
       en: "Fresh seaweed salad with sesame dressing",
     },
-    categoryId: 4,
-    color: "bg-lime-400"
+    category: "fusion_dishes",
+    color: "bg-lime-400",
   },
   {
-    name:{
+    id: "20t",
+    name: {
       es: "Nigiri de Camarón",
       en: "Shrimp Nigiri",
-    } ,
+    },
     price: 4.99,
     img: "nigiri_shrimp.jpg",
     description: {
-      es:"Láminas de camarón sobre arroz sazonado",
+      es: "Láminas de camarón sobre arroz sazonado",
       en: "Shrimp slices over seasoned rice",
     },
-    categoryId: 4,
-    color: "bg-lime-400"
+    category: "fusion_dishes",
+    color: "bg-lime-400",
   },
   {
-    name:{
+    id: "21u",
+    name: {
       es: "Sashimi de Salmón",
       en: "Salmon Sashimi",
-    } ,
+    },
     price: 9.99,
     img: "sashimi_salmon.jpg",
     description: {
-      es:"Láminas frescas de salmón para disfrutar crudo",
+      es: "Láminas frescas de salmón para disfrutar crudo",
       en: "Fresh slices of salmon to enjoy raw",
     },
-    categoryId: 5,
-    color: "bg-amber-300"
+    category: "signature_sushi",
+    color: "bg-amber-300",
   },
   {
-    name:{
+    id: "22v",
+    name: {
       es: "Maki de Aguacate",
       en: "Avocado Maki",
-    } ,
+    },
     price: 5.99,
     img: "maki_avocado_sushi.jpg",
     description: {
-      es:"Maki de aguacate fresco envuelto en alga nori y arroz",
+      es: "Maki de aguacate fresco envuelto en alga nori y arroz",
       en: "Fresh avocado wrapped in nori seaweed and rice",
     },
-    categoryId: 5,
-    color: "bg-amber-300"
+    category: "signature_sushi",
+    color: "bg-amber-300",
   },
   {
-    name:{
+    id: "23w",
+    name: {
       es: "Uramaki de Langostino",
       en: "Shrimp Uramaki",
-    } ,
+    },
     price: 8.99,
     img: "urumaki_big_shrimp.jpg",
     description: {
-      es:"Uramaki relleno de langostino cocido y aguacate",
+      es: "Uramaki relleno de langostino cocido y aguacate",
       en: "Uramaki filled with cooked shrimp and avocado",
     },
-    categoryId: 5,
-    color: "bg-amber-300"
+    category: "signature_sushi",
+    color: "bg-amber-300",
   },
   {
-    name:{
+    id: "24x",
+    name: {
       es: "Nigiri de Trucha",
       en: "Trout Nigiri",
-    } ,
+    },
     price: 4.99,
     img: "nigiri_trout.jpg",
     description: {
-      es:"Láminas de trucha fresca sobre arroz sazonado",
+      es: "Láminas de trucha fresca sobre arroz sazonado",
       en: "Fresh trout slices over seasoned rice",
     },
-    categoryId: 5,
-    color: "bg-amber-300"
+    category: "signature_sushi",
+    color: "bg-amber-300",
   },
   {
-    name:{
+    id: "25y",
+    name: {
       es: "Temaki de Atún Picante",
       en: "Spicy Tuna Temaki",
-    } ,
+    },
     price: 6.99,
     img: "temaki_atun_spicy.jpg",
     description: {
-      es:"Cono de alga nori relleno de atún picante y vegetales",
+      es: "Cono de alga nori relleno de atún picante y vegetales",
       en: "Nori seaweed cone filled with spicy tuna and vegetables",
     },
-    categoryId: 5,
-    color: "bg-amber-300"
+    category: "signature_sushi",
+    color: "bg-amber-300",
   },
   {
-    name:{
+    id: "26z",
+    name: {
       es: "Rollito de Salmón",
       en: "Salmon Roll",
-    } ,
+    },
     price: 7.99,
     img: "temaki_atun_spicy_2.jpg",
     description: {
-      es:"Rollito de salmón fresco, aguacate y queso crema",
+      es: "Rollito de salmón fresco, aguacate y queso crema",
       en: "Fresh salmon, avocado, and cream cheese roll",
     },
-    categoryId: 6,
-    color: "bg-indigo-500"
+    category: "special_rolls",
+    color: "bg-indigo-500",
   },
   {
-    name:{
+    id: "27aa",
+    name: {
       es: "Nigiri de Pez Mantequilla",
       en: "Yellowtail Nigiri",
-    } ,
+    },
     price: 10.99,
     img: "temaki_atun_spicy_3.jpg",
     description: {
-      es:"Láminas de pez mantequilla fresco sobre arroz sazonado",
+      es: "Láminas de pez mantequilla fresco sobre arroz sazonado",
       en: "Fresh yellowtail slices over seasoned rice",
     },
-    categoryId: 6,
-    color: "bg-indigo-500"
+    category: "special_rolls",
+    color: "bg-indigo-500",
   },
   {
-    name:{
+    id: "28bb",
+    name: {
       es: "Maki de Mango",
       en: "Mango Maki",
-    } ,
+    },
     price: 5.99,
     img: "maki_mango_sushi.jpg",
     description: {
-      es:"Maki de mango fresco envuelto en alga nori y arroz",
+      es: "Maki de mango fresco envuelto en alga nori y arroz",
       en: "Fresh mango wrapped in nori seaweed and rice",
     },
-    categoryId: 6,
-    color: "bg-indigo-500"
+    category: "special_rolls",
+    color: "bg-indigo-500",
   },
   {
-    name:{
+    id: "29cc",
+    name: {
       es: "Uramaki de Espárragos",
       en: "Asparagus Uramaki",
-    } ,
+    },
     price: 8.99,
     img: "urumaki_tempura.jpg",
     description: {
-      es:"Uramaki relleno de espárragos frescos y aguacate",
+      es: "Uramaki relleno de espárragos frescos y aguacate",
       en: "Uramaki filled with fresh asparagus and avocado",
     },
-    categoryId: 6,
-    color: "bg-indigo-500"
+    category: "special_rolls",
+    color: "bg-indigo-500",
   },
   {
-    name:{
+    id: "30dd",
+    name: {
       es: "Nigiri de Anguila",
       en: "Eel Nigiri",
-    } ,
+    },
     price: 7.99,
     img: "niguiri_anguila.jpg",
     description: {
-      es:"Láminas de anguila a la parrilla sobre arroz sazonado",
+      es: "Láminas de anguila a la parrilla sobre arroz sazonado",
       en: "Grilled eel slices over seasoned rice",
     },
-    categoryId: 6,
-    color: "bg-indigo-500"
-  }
-]
+    category: "special_rolls",
+    color: "bg-indigo-500",
+  },
+];
+
 export const categorias = [
   {
     name: {
       es: "Sushi Rolls",
-      en: "Sushi Rolls"
+      en: "Sushi Rolls",
     },
-    value: "sushi_rolls"
+    value: "sushi_rolls",
   },
   {
     name: {
       es: "Entradas Calientes",
-      en: "Hot Appetizers"
+      en: "Hot Appetizers",
     },
-    value: "hot_appetizers"
+    value: "hot_appetizers",
   },
   {
     name: {
       es: "Especialidades de Sashimi",
-      en: "Sashimi Specialties"
+      en: "Sashimi Specialties",
     },
-    value: "sashimi_specialties"
+    value: "sashimi_specialties",
   },
   {
     name: {
       es: "Platos Fusionados",
-      en: "Fusion Dishes"
+      en: "Fusion Dishes",
     },
-    value: "fusion_dishes"
+    value: "fusion_dishes",
   },
   {
     name: {
       es: "Sushi de Autor",
-      en: "Signature Sushi"
+      en: "Signature Sushi",
     },
-    value: "signature_sushi"
+    value: "signature_sushi",
   },
   {
     name: {
       es: "Rolls Especiales",
-      en: "Special Rolls"
+      en: "Special Rolls",
     },
-    value: "special_rolls"
-  }
+    value: "special_rolls",
+  },
 ];
 
-
-export const ASSETS_BASE_URI ='/assets/img/Firefly_'
+export const ASSETS_BASE_URI = "/assets/img/Firefly_";
